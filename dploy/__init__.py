4e9d80c61754a26415aa53902a7ecb10688c19b1
Ryan Carney <arecarn@gmail.com>
2016-03-11 18:53:58 -0800

    start making dploy work like gnu stow
    GNU stow is probably the best tool for deploying dotfiles but it has one
    problem (as far as I know) it doesn't work on windows.

--- dploy/__init__.py
+++ dploy/__init__.py
@@ -9,75 +9,48 @@ import os
 import shutil
 
 from dploy.util import resolve_abs_path
-from dploy.util import resolve_path
-from dploy.util import time_stamped
-from dploy.util import get_absolute_dirname
 
-def backup(dotfiles, dotfiles_directory):
+def link_file(target, dest):
+    dest_dir = os.path.dirname(dest)
+    # get a relative path to the target from the destination location of
+    # the file, this way we will have a relative symlink
+    target_relative = os.path.relpath(target, start=dest_dir)
 
-    dotfiles_directory_name = os.path.basename(dotfiles_directory)
+    try:
+        os.symlink(target_relative, dest)
+    except Exception as exception_message:
+        print(exception_message)
 
-    backup_location = get_absolute_dirname(dotfiles_directory)
+def deploy_files(target, dest):
+    target_absolute = resolve_abs_path(target)
+    dest_absolute = resolve_abs_path(dest)
 
-    backup_dir = os.path.join(backup_location, time_stamped(dotfiles_directory_name))
+    print("{dest_absolute} = dest_absolute".format(dest_absolute=dest_absolute))
+    print("{target_absolute} = target_absolute".format(target_absolute=target_absolute))
 
-    print("Backing Up Old Dotfiles Into {0}".format(backup_dir))
-    os.mkdir(backup_dir)
+    if os.path.islink(dest_absolute):
+        link_location = os.readlink(dest_absolute)
+        print("dest is a link that points to {link_location}".format(
+            link_location=link_location))
 
-    for dotfile in dotfiles:
-        target = resolve_abs_path(dotfile["location"])
+        print("dest is a link that points to {link_location}".format(
+            link_location=resolve_abs_path(link_location)))
 
-        dotfile_basename = os.path.basename(dotfile["location"])
-        dest = os.path.join(backup_dir, dotfile_basename)
-
-        try:
-            print("Moving {target} into {dest}".format(target=target, dest=dest))
-            shutil.move(target, dest)
-        except Exception as exception_message:
-            print(exception_message)
-
-
-def symlink_files(dotfiles, dotfiles_directory):
-    print("Symbolic Linking Files")
-
-    for dotfile in dotfiles:
-
-        dest = resolve_abs_path(dotfile["location"])
-
-        dotfile_location_directory = os.path.dirname(dest)
-
-        source_name = resolve_path(dotfile["source"])
-        source_path = os.path.join(dotfiles_directory, source_name)
-        source_path_absolute = resolve_abs_path(source_path)
-
-        # get a relative path to the source from the destination location of
-        # the dotfile, this way we will have a relative symlink
-        source_path_relative = os.path.relpath(source_path_absolute,
-                                               start=dotfile_location_directory)
-
-        print("Creating link {dest} pointing to {source}".format(
-            source=source_path_relative,
+        if resolve_abs_path(os.readlink(dest_absolute)) == target_absolute:
+            print("already linked")
+        else:
+            print("Abort other link exits")
+    elif os.path.isfile(dest_absolute):
+        print("Abort File Already Exists")
+    elif os.path.isdir(dest_absolute):
+        for file in os.listdir(target_absolute):
+            link_file(os.path.join(target_absolute, file),
+                      os.path.join(dest_absolute, file))
+    else:
+        link_file(target_absolute, dest_absolute)
+        print("Creating link {dest} pointing to {target}".format(
+            target=target,
             dest=dest))
-
-        try:
-            os.symlink(source_path_relative, dest)
-        except Exception as exception_message:
-            print(exception_message)
-
-
-def create_files(config_files):
-    print("Creating Local Config Files")
-
-    for config_file in config_files:
-        config_file = resolve_abs_path(config_file)
-        print("Creating config file if it doesn't already exist: {0}".format(config_file))
-
-        try:
-            with open(config_file, "a+") as file:
-                pass
-        except Exception as exception_message:
-            print(exception_message)
-
 
 def create_directories(directories):
     print("Creating Directories")
@@ -90,5 +63,3 @@ def create_directories(directories):
             os.makedirs(directory)
         except Exception as exception_message:
             print(exception_message)
-
-
--- dploy/__main__.py
+++ dploy/__main__.py
@@ -10,33 +10,21 @@ import dploy
 from dploy.util import dynamic_import
 
 PARSER = argparse.ArgumentParser(description='dploy dotfiles')
-PARSER.add_argument("--file",
-                    default="~/dotfiles/setup_config.py",
-                    help="path of the dploy file")
+PARSER.add_argument('target',
+                    help='target or source of the files to dploy')
+PARSER.add_argument('dest',
+                    nargs='+',
+                    help='dest path to dploy file')
 
 ARGS = PARSER.parse_args()
-
-SETUP_CONFIG = dynamic_import(ARGS.file, "")
-
 
 def main():
     """
     Main entry function
     """
 
-    dploy.backup(SETUP_CONFIG.DOTFILES,
-                 SETUP_CONFIG.DOTFILES_DIRECTORY)
-    print("\n")
-
-    dploy.symlink_files(SETUP_CONFIG.DOTFILES,
-                        SETUP_CONFIG.DOTFILES_DIRECTORY)
-    print("\n")
-
-    dploy.create_files(SETUP_CONFIG.FILES)
-    print("\n")
-
-    dploy.create_directories(SETUP_CONFIG.DIRECTORIES)
-    print("\n")
+    for dest in ARGS.dest:
+        dploy.deploy_files(ARGS.target, dest)
 
 if __name__ == "__main__":
     main()
