cdc7ede50faac7f6b5028ad6958af64b03495bc7
Ryan Carney <arecarn@gmail.com>
2016-03-13 09:28:41 -0700

    dploy script: allow only one dest argument
    This change was made to simplify the script since more changes are bound to be
    made.

--- dploy/__main__.py
+++ dploy/__main__.py
@@ -12,18 +12,7 @@ PARSER = argparse.ArgumentParser(description='dploy dotfiles')
 PARSER.add_argument('target',
                     help='target or source of the files to dploy')
 PARSER.add_argument('dest',
-                    nargs='+',
                     help='dest path to dploy file')
-
 ARGS = PARSER.parse_args()
 
-def main():
-    """
-    Main entry function
-    """
-
-    for dest in ARGS.dest:
-        dploy.dploy(ARGS.target, dest)
-
-if __name__ == "__main__":
-    main()
+dploy.dploy(ARGS.target, ARGS.dest)
