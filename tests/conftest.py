cdec2fcbd12afa5836be51b935f354b170fb0f1f
Ryan Carney <arecarn@gmail.com>
2016-04-02 12:38:16 -0700

    test: move fixtures to conftest.py from test_stow.py
    this is so other test modules can also use the same fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,95 @@
+import pytest
+import util
+
+@pytest.fixture()
+def source_a(request):
+    name = 'source_a'
+    tree = [
+        {
+            name : [
+                {
+                    'aaa': [
+                        'aaa',
+                        'bbb',
+                        {
+                            'ccc': [
+                                'aaa',
+                                'bbb',
+                            ],
+                        },
+                    ],
+                },
+            ],
+        },
+    ]
+    util.create_tree(tree)
+
+    def source_a_teardown():
+        util.remove_tree(name)
+    request.addfinalizer(source_a_teardown)
+
+
+@pytest.fixture()
+def source_b(request):
+    name = 'source_b'
+    tree = [
+        {
+            name : [
+                {
+                    'aaa': [
+                        'ddd',
+                        'eee',
+                        {
+                            'fff': [
+                                'aaa',
+                                'bbb',
+                            ],
+                        },
+                    ],
+                },
+            ],
+        },
+    ]
+    util.create_tree(tree)
+
+    def source_b_teardown():
+        util.remove_tree(name)
+    request.addfinalizer(source_b_teardown)
+
+
+@pytest.fixture()
+def source_c(request):
+    name = 'source_c'
+    tree = [
+        {
+            name : [
+                {
+                    'aaa': [
+                        'aaa',
+                        'bbb',
+                        {
+                            'ccc': [
+                                'aaa',
+                                'bbb',
+                            ],
+                        },
+                    ],
+                },
+            ],
+        },
+    ]
+    util.create_tree(tree)
+
+    def source_c_teardown():
+        util.remove_tree(name)
+    request.addfinalizer(source_c_teardown)
+
+
+@pytest.fixture()
+def dest(request):
+    name = 'dest'
+    util.create_directory(name)
+
+    def dest_teardown():
+        util.remove_tree(name)
+    request.addfinalizer(dest_teardown)
