14993bb6b6934a68121a227094b333def058b42c
Ryan Carney <arecarn@gmail.com>
2016-04-05 22:54:12 -0700

    __main__.py: put functionality in a function
    this way setup.py can create and entry point pointing at the function to call
    dploy

--- dploy/__main__.py
+++ dploy/__main__.py
@@ -7,33 +7,34 @@ The main entry point to the dploy script
 import argparse
 import dploy.version
 
-PARSER = argparse.ArgumentParser(prog='dploy')
-PARSER.add_argument('--version',
-                    action='version',
-                    version='%(prog)s {version}'.format(version=dploy.version.__version__))
+def main():
+    PARSER = argparse.ArgumentParser(prog='dploy')
+    PARSER.add_argument('--version',
+                action='version',
+                version='%(prog)s {version}'.format(version=dploy.version.__version__))
 
-SUB_PARSERS = PARSER.add_subparsers(dest="sub_command")
+    SUB_PARSERS = PARSER.add_subparsers(dest="sub_command")
 
-STOW_PARSER = SUB_PARSERS.add_parser('stow')
-STOW_PARSER.add_argument('source',
-                         nargs='+',
-                         help='source directory to stow')
-STOW_PARSER.add_argument('dest',
-                         help='destination path to stow into')
+    STOW_PARSER = SUB_PARSERS.add_parser('stow')
+    STOW_PARSER.add_argument('source',
+                 nargs='+',
+                 help='source directory to stow')
+    STOW_PARSER.add_argument('dest',
+                 help='destination path to stow into')
 
-LINK_PARSER = SUB_PARSERS.add_parser('link')
-LINK_PARSER.add_argument('source',
-                         help='source files or directories to link')
-LINK_PARSER.add_argument('dest',
-                         help='destination path to link')
+    LINK_PARSER = SUB_PARSERS.add_parser('link')
+    LINK_PARSER.add_argument('source',
+                 help='source files or directories to link')
+    LINK_PARSER.add_argument('dest',
+                 help='destination path to link')
 
-ARGS = PARSER.parse_args()
+    ARGS = PARSER.parse_args()
 
-if ARGS.sub_command != None:
-    if ARGS.sub_command == 'stow':
-        for source in ARGS.source:
-            dploy.stow(source, ARGS.dest)
-    elif ARGS.sub_command == 'link':
-        dploy.link(ARGS.source, ARGS.dest)
-else:
-    PARSER.print_help()
+    if ARGS.sub_command != None:
+        if ARGS.sub_command == 'stow':
+            for source in ARGS.source:
+                dploy.stow(source, ARGS.dest)
+        elif ARGS.sub_command == 'link':
+            dploy.link(ARGS.source, ARGS.dest)
+    else:
+        PARSER.print_help()
