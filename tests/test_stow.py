36fb19445b7f4f012242206ebf2f92bd3e82efef
Ryan Carney <arecarn@gmail.com>
2016-04-18 08:14:38 -0700

    stow:test: replace islink()/samefile() /w readlink() assert
    This change simplifies the tests as well as improving coverage by verifying
    that the links are made in an expected way.

--- tests/test_stow.py
+++ tests/test_stow.py
@@ -13,7 +13,7 @@ def test_stow_basic(source_a, dest):
     """
     # pylint: disable=unused-argument
     dploy.stow(['source_a'], 'dest')
-    assert os.path.islink('dest/aaa')
+    assert os.readlink('dest/aaa') == '../source_a/aaa'
 
 
 def test_stow_the_same_tree_twice(source_a, dest):
@@ -23,7 +23,7 @@ def test_stow_the_same_tree_twice(source_a, dest):
     # pylint: disable=unused-argument
     dploy.stow(['source_a'], 'dest')
     dploy.stow(['source_a'], 'dest')
-    assert os.path.islink('dest/aaa')
+    assert os.readlink('dest/aaa') == '../source_a/aaa'
 
 
 def test_stow_unfolding_basic(source_a, source_b, dest):
@@ -32,7 +32,7 @@ def test_stow_unfolding_basic(source_a, source_b, dest):
     """
     # pylint: disable=unused-argument
     dploy.stow(['source_a'], 'dest')
-    assert os.path.islink('dest/aaa')
+    assert os.readlink('dest/aaa') == '../source_a/aaa'
 
     assert os.path.isfile('dest/aaa/aaa')
     assert os.path.isfile('dest/aaa/bbb')
@@ -41,19 +41,13 @@ def test_stow_unfolding_basic(source_a, source_b, dest):
     dploy.stow(['source_b'], 'dest')
     assert os.path.isdir('dest/aaa')
 
-    assert os.path.islink('dest/aaa/aaa')
-    assert os.path.samefile('dest/aaa/aaa','source_a/aaa/aaa')
-    assert os.path.islink('dest/aaa/bbb')
-    assert os.path.samefile('dest/aaa/bbb','source_a/aaa/bbb')
-    assert os.path.islink('dest/aaa/ccc')
-    assert os.path.samefile('dest/aaa/ccc','source_a/aaa/ccc')
+    assert os.readlink('dest/aaa/aaa') == '../../source_a/aaa/aaa'
+    assert os.readlink('dest/aaa/bbb') == '../../source_a/aaa/bbb'
+    assert os.readlink('dest/aaa/ccc') == '../../source_a/aaa/ccc'
 
-    assert os.path.islink('dest/aaa/ddd')
-    assert os.path.samefile('dest/aaa/ddd','source_b/aaa/ddd')
-    assert os.path.islink('dest/aaa/eee')
-    assert os.path.samefile('dest/aaa/eee','source_b/aaa/eee')
-    assert os.path.islink('dest/aaa/fff')
-    assert os.path.samefile('dest/aaa/fff','source_b/aaa/fff')
+    assert os.readlink('dest/aaa/ddd') == '../../source_b/aaa/ddd'
+    assert os.readlink('dest/aaa/eee') == '../../source_b/aaa/eee'
+    assert os.readlink('dest/aaa/fff') == '../../source_b/aaa/fff'
 
 def test_unfoling_with_conflicts(source_a, source_c, dest):
     """
