836261a3589d50752f23c56b061c7c091280f1c9
Ryan Carney <arecarn@gmail.com>
2016-05-03 12:59:04 -0700

    pylint: re-enable unused-argument warning in tests
    all the arguemnts in the tests are now used so this pylint warning can be
    re-enabled

--- tests/test_stow.py
+++ tests/test_stow.py
@@ -13,20 +13,17 @@ import util
 
 
 def test_stow_with_basic_senario(source_a, dest):
-    # pylint: disable=unused-argument
     dploy.stow([source_a], dest)
     assert os.readlink(os.path.join(dest, 'aaa')) == os.path.join('..', 'source_a', 'aaa')
 
 
 def test_stow_with_the_same_tree_twice(source_a, dest):
-    # pylint: disable=unused-argument
     dploy.stow([source_a], dest)
     dploy.stow([source_a], dest)
     assert os.readlink(os.path.join(dest, 'aaa')) == os.path.join('..', 'source_a', 'aaa')
 
 
 def test_stow_unfolding_with_two_invocations(source_a, source_b, dest):
-    # pylint: disable=unused-argument
     dploy.stow([source_a], dest)
     assert os.readlink(os.path.join(dest, 'aaa')) == os.path.join('..', 'source_a', 'aaa')
 
@@ -53,7 +50,6 @@ def test_stow_unfolding_with_two_invocations(source_a, source_b, dest):
 
 
 def test_stow_unfolding_with_mutliple_sources(source_a, source_b, dest):
-    # pylint: disable=unused-argument
     dploy.stow([source_a, source_b], dest)
 
     assert os.path.isdir(os.path.join(dest, 'aaa'))
@@ -74,7 +70,6 @@ def test_stow_unfolding_with_mutliple_sources(source_a, source_b, dest):
 
 
 def test_stow_with_existing_file_conflicts(source_a, source_c, dest):
-    # pylint: disable=unused-argument
     dploy.stow([source_a], dest)
 
     with pytest.raises(SystemExit):
@@ -82,7 +77,6 @@ def test_stow_with_existing_file_conflicts(source_a, source_c, dest):
 
 
 def test_stow_with_existing_broken_link(source_a, dest):
-    # pylint: disable=unused-argument
     os.symlink('non_existant_source', os.path.join(dest, 'aaa'))
 
     with pytest.raises(SystemExit):
@@ -90,37 +84,31 @@ def test_stow_with_existing_broken_link(source_a, dest):
 
 
 def test_stow_with_source_conflicts(source_a, source_c, dest):
-    # pylint: disable=unused-argument
     with pytest.raises(SystemExit):
         dploy.stow([source_a, source_c], dest)
 
 
 def test_stow_with_non_existant_source(dest):
-    # pylint: disable=unused-argument
     with pytest.raises(SystemExit):
         dploy.stow(['source'], dest)
 
 
 def test_stow_with_non_existant_dest(source_a):
-    # pylint: disable=unused-argument
     with pytest.raises(SystemExit):
         dploy.stow([source_a], 'dest')
 
 
 def test_stow_with_file_as_source(file_a, dest):
-    # pylint: disable=unused-argument
     with pytest.raises(SystemExit):
         dploy.stow([file_a], dest)
 
 
 def test_stow_with_file_as_dest(source_a, file_a):
-    # pylint: disable=unused-argument
     with pytest.raises(SystemExit):
         dploy.stow([source_a], file_a)
 
 
 def test_stow_with_file_as_dest_and_source(file_a, file_b):
-    # pylint: disable=unused-argument
     with pytest.raises(SystemExit):
         dploy.stow([file_a], 'file_b')
 
