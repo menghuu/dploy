eb8e2970dd021437f41b448d837f036416a0b476
Ryan Carney <arecarn@gmail.com>
2016-05-16 13:11:53 -0700

    break out the cli interface into it's own module
    This was done so the cli interface could be integrated into the tests

--- dploy/__main__.py
+++ dploy/__main__.py
@@ -1,76 +1,16 @@
 #!/usr/bin/env python3
 
 """
-The main entry point to the dploy script
+The entry point when dploy is called as a module
 """
 
-import sys
-import argparse
-import dploy
-import dploy.version
+import dploy.cli
 
 def main():
     """
-    todo
+    main entry point when using dploy from the command line
     """
-
-    parser = argparse.ArgumentParser(prog='dploy')
-    parser.add_argument('--version',
-                        action='version',
-                        version='%(prog)s {version}'.format(version=dploy.version.__version__))
-
-    sub_parsers = parser.add_subparsers(dest="sub_command")
-
-    stow_parser = sub_parsers.add_parser('stow')
-    stow_parser.add_argument('source',
-                             nargs='+',
-                             help='source directory to stow')
-    stow_parser.add_argument('dest',
-                             help='destination path to stow into')
-
-    stow_parser = sub_parsers.add_parser('unstow')
-    stow_parser.add_argument('source',
-                             nargs='+',
-                             help='source directory to unstow from')
-    stow_parser.add_argument('dest',
-                             help='destination path to unstow')
-
-    link_parser = sub_parsers.add_parser('link')
-    link_parser.add_argument('source',
-                             help='source file or directory to link')
-    link_parser.add_argument('dest',
-                             help='destination path to link')
-
-    args = parser.parse_args()
-
-    if args.sub_command != None:
-        if args.sub_command == 'stow':
-            try:
-                dploy.stow(args.source, args.dest)
-            except (ValueError, PermissionError) as error:
-                print(error, file=sys.stderr)
-                sys.exit(1)
-
-
-        elif args.sub_command == 'unstow':
-            try:
-                dploy.unstow(args.source, args.dest)
-            except (ValueError, PermissionError) as error:
-                print(error, file=sys.stderr)
-                sys.exit(1)
-
-        elif args.sub_command == 'link':
-            try:
-                dploy.link(args.source, args.dest)
-            except (ValueError, PermissionError) as error:
-                print(error, file=sys.stderr)
-                sys.exit(1)
-
-        else:
-            parser.print_help()
-    else:
-        parser.print_help()
-
+    dploy.cli.run()
 
 if __name__ == '__main__':
     main()
---
+++ dploy/cli.py
@@ -0,0 +1,79 @@
+"""
+The command line interface
+"""
+
+import sys
+import argparse
+import dploy
+import dploy.version
+
+
+def create_parser():
+    """
+    create the CLI argument parser
+    """
+    parser = argparse.ArgumentParser(prog='dploy')
+    parser.add_argument('--version',
+                        action='version',
+                        version='%(prog)s {version}'.format(version=dploy.version.__version__))
+
+    sub_parsers = parser.add_subparsers(dest="sub_command")
+
+    stow_parser = sub_parsers.add_parser('stow')
+    stow_parser.add_argument('source',
+                             nargs='+',
+                             help='source directory to stow')
+    stow_parser.add_argument('dest',
+                             help='destination path to stow into')
+
+    stow_parser = sub_parsers.add_parser('unstow')
+    stow_parser.add_argument('source',
+                             nargs='+',
+                             help='source directory to unstow from')
+    stow_parser.add_argument('dest',
+                             help='destination path to unstow')
+
+    link_parser = sub_parsers.add_parser('link')
+    link_parser.add_argument('source',
+                             help='source file or directory to link')
+    link_parser.add_argument('dest',
+                             help='destination path to link')
+    return parser
+
+
+def run(arguments=None):
+    """
+    interpret the parser arguments and execute the corresponding commands
+    """
+    parser = create_parser()
+
+    if arguments is None:
+        args = parser.parse_args()
+    else:
+        args = parser.parse_args(arguments)
+
+
+    if args.sub_command == 'stow':
+        try:
+            dploy.stow(args.source, args.dest)
+        except (ValueError, PermissionError) as error:
+            print(error, file=sys.stderr)
+            sys.exit(1)
+
+
+    elif args.sub_command == 'unstow':
+        try:
+            dploy.unstow(args.source, args.dest)
+        except (ValueError, PermissionError) as error:
+            print(error, file=sys.stderr)
+            sys.exit(1)
+
+    elif args.sub_command == 'link':
+        try:
+            dploy.link(args.source, args.dest)
+        except (ValueError, PermissionError) as error:
+            print(error, file=sys.stderr)
+            sys.exit(1)
+
+    else:
+        parser.print_help()
