dafca354f7525e7725de71ee475de8d093a9cf43
Ryan Carney <arecarn@gmail.com>
2016-10-01 10:40:50 -0700

    errors: make formatted message available to tests

--- dploy/errors.py
+++ dploy/errors.py
@@ -11,64 +11,76 @@ ERROR_HEAD = 'dploy {subcmd}: can not {subcmd} '
 class SourceIsSameAsDest():
     def __init__(self, subcmd, file):
         self.msg = ERROR_HEAD + "'{file}': A source argument is the same as the dest argument"
-        self.exception = ValueError(self.msg.format(subcmd=subcmd, file=file))
+        self.msg = self.msg.format(subcmd=subcmd, file=file)
+        self.exception = ValueError(self.msg)
 
 
 class ConflictsWithAnotherSource():
     def __init__(self, subcmd, files):
         self.msg = ERROR_HEAD + "the following: Conflicts with other source {files}"
         files_list = '\n    '  + '\n    '.join(files)
-        self.exception = ValueError(self.msg.format(subcmd=subcmd, files=files_list))
+        self.msg = self.msg.format(subcmd=subcmd, files=files_list)
+        self.exception = ValueError(self.msg)
 
 
 class ConflictsWithExistingFile():
     def __init__(self, subcmd, source, dest):
         self.msg = ERROR_HEAD + "'{source}': Conflicts with existing file '{dest}'"
-        self.exception = ValueError(self.msg.format(subcmd=subcmd, source=source, dest=dest))
+        self.msg = self.msg.format(subcmd=subcmd, source=source, dest=dest)
+        self.exception = ValueError(self.msg)
 
 
 class ConflictsWithExistingLink():
     def __init__(self, subcmd, source, dest):
         self.msg = ERROR_HEAD + "'{source}': Conflicts with existing symlink '{dest}'"
-        self.exception = ValueError(self.msg.format(subcmd=subcmd, source=source, dest=dest))
+        self.msg = self.msg.format(subcmd=subcmd, source=source, dest=dest)
+        self.exception = ValueError(self.msg)
 
 
 class InsufficientPermissions():
     def __init__(self, subcmd, file):
         self.msg = ERROR_HEAD + "'{file}': Insufficient permissions"
-        self.exception = PermissionError(self.msg.format(subcmd=subcmd, file=file))
+        self.msg = self.msg.format(subcmd=subcmd, file=file)
+        self.exception = PermissionError(self.msg)
 
 
 class NoSuchDirectory():
     def __init__(self, subcmd, file):
         self.msg = ERROR_HEAD + "'{file}': No such directory"
-        self.exception = NotADirectoryError(self.msg.format(subcmd=subcmd, file=file))
+        self.msg = self.msg.format(subcmd=subcmd, file=file)
+        self.exception = NotADirectoryError(self.msg)
 
 
 class PermissionDenied():
     def __init__(self, subcmd, file):
         self.msg = ERROR_HEAD + "'{file}': Permission denied"
-        self.exception = PermissionError(self.msg.format(subcmd=subcmd, file=file))
+        self.msg = self.msg.format(subcmd=subcmd, file=file)
+        self.exception = PermissionError(self.msg)
+
 
 class InsufficientPermissionsToSubcmdFrom():
     def __init__(self, subcmd, file):
         self.msg = ERROR_HEAD + "from '{file}': Insufficient permissions"
-        self.exception = PermissionError(self.msg.format(subcmd=subcmd, file=file))
+        self.msg = self.msg.format(subcmd=subcmd, file=file)
+        self.exception = PermissionError(self.msg)
 
 
 class NoSuchDirectoryToSubcmdInto():
     def __init__(self, subcmd, file):
         self.msg = ERROR_HEAD + "into '{file}': No such directory"
-        self.exception = NotADirectoryError(self.msg.format(subcmd=subcmd, file=file))
+        self.msg = self.msg.format(subcmd=subcmd, file=file)
+        self.exception = NotADirectoryError(self.msg)
 
 
 class InsufficientPermissionsToSubcmdTo():
     def __init__(self, subcmd, file):
         self.msg = ERROR_HEAD + "to '{file}': Insufficient permissions"
-        self.exception = PermissionError(self.msg.format(subcmd=subcmd, file=file))
+        self.msg = self.msg.format(subcmd=subcmd, file=file)
+        self.exception = PermissionError(self.msg)
 
 
 class NoSuchFileOrDirectory():
     def __init__(self, subcmd, file):
         self.msg = ERROR_HEAD + "'{file}': No such file or directory"
-        self.exception = FileNotFoundError(self.msg.format(subcmd=subcmd, file=file))
+        self.msg = self.msg.format(subcmd=subcmd, file=file)
+        self.exception = FileNotFoundError(self.msg)
