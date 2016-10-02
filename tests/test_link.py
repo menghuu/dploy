4dc435b61ecd89442c813b15f9e681914a5be877
Ryan Carney <arecarn@gmail.com>
2016-10-01 19:27:19 -0700

    link: have tests check contents of for error messages
    This way we can verify the error messages reported to users

--- tests/test_link.py
+++ tests/test_link.py
@@ -8,8 +8,10 @@ Tests for the link sub command
 import os
 import pytest
 import dploy
+import dploy.errors as errors
 import utils
 
+SUBCMD = 'link'
 
 def test_link_with_directory_as_source(source_a, dest):
     dploy.link(source_a, os.path.join(dest, 'source_a_link'))
@@ -22,29 +24,40 @@ def test_link_with_file_as_source(file_a, dest):
 
 
 def test_link_with_non_existant_source(dest):
-    with pytest.raises(FileNotFoundError):
-        dploy.link('source_a', os.path.join(dest, 'source_a_link'))
+    non_existant_source = 'source_a'
+    with pytest.raises(FileNotFoundError) as e:
+        dploy.link(non_existant_source, os.path.join(dest, 'source_a_link'))
+    assert errors.NoSuchFileOrDirectory(subcmd=SUBCMD, file=non_existant_source).msg in str(e.value)
 
 
 def test_link_with_non_existant_dest(source_a):
-    with pytest.raises(FileNotFoundError):
-        dploy.link(source_a, os.path.join('dest', 'source_a_link'))
+    non_existant_dest = 'dest'
+    with pytest.raises(FileNotFoundError) as e:
+        dploy.link(source_a, os.path.join(non_existant_dest, 'source_a_link'))
+    assert errors.NoSuchFileOrDirectory(subcmd=SUBCMD, file=non_existant_dest).msg in str(e.value)
 
 
 def test_link_with_read_only_dest(file_a, dest):
+    dest_file = os.path.join(dest, 'file_a_link')
     utils.remove_write_permission(dest)
-    with pytest.raises(PermissionError):
-        dploy.link(file_a, os.path.join(dest, 'file_a_link'))
+    with pytest.raises(PermissionError) as e:
+        dploy.link(file_a, dest_file)
+    assert (errors.InsufficientPermissionsToSubcmdTo(subcmd=SUBCMD, file=dest_file).msg
+            in str(e.value))
 
 
 def test_link_with_write_only_source(file_a, dest):
+    dest_file = os.path.join(dest, 'file_a_link')
     utils.remove_read_permission(file_a)
-    with pytest.raises(PermissionError):
-        dploy.link(file_a, os.path.join(dest, 'file_a_link'))
+    with pytest.raises(PermissionError) as e:
+        dploy.link(file_a, dest_file)
+    assert errors.InsufficientPermissions(subcmd=SUBCMD, file=file_a).msg in str(e.value)
 
 
 def test_link_with_conflicting_broken_link_at_dest(file_a, dest):
-    os.symlink('non_existant_source', os.path.join(dest, 'file_a_link'))
+    dest_file = os.path.join(dest, 'file_a_link')
+    os.symlink('non_existant_source', dest_file)
     with pytest.raises(ValueError) as e:
-        dploy.link(file_a, os.path.join(dest, 'file_a_link'))
-    assert utils.is_subcmd_error_message('link', e)
+        dploy.link(file_a, dest_file)
+    assert (errors.ConflictsWithExistingLink(subcmd=SUBCMD, source=file_a, dest=dest_file).msg
+            in str(e.value))
